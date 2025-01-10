6960ed0cd7770c2b7ee943c726fec2ab3bb68387
Shaf3eii <elshafeim45@gmail.com>
2025-01-10 23:47:59 +0200

    ShaFeiii Here

---
+++ January/09_Counting_Words_With_a_Given_Prefix/ShaFeiii.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// c++ Solution
+class Solution {
+public:
+    int prefixCount(vector<string>& words, string pref) {
+        int ans = 0;
+        for (int i = 0; i < (int)words.size(); ++i) {
+            if (words[i].find(pref) == 0) ans++;
+        }
+        return ans;
+    }
+};
+
+int main() {
+
+    return 0;
+}
